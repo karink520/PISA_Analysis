bc45130a4ac2600a693788c924cb052d65874579
karin <karink520@gmail.com>
2014-01-05 14:29:37 -0600

    Created DataFile class with head method

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -1,11 +1,23 @@
 import sys
+import os
 
 __author__ = 'karin'
-
+DATA_FOLDER = '../data'
+DATA_FILES = [os.path.join(DATA_FOLDER, filename) for filename in os.listdir(DATA_FOLDER)]
 
 def __main():
-    pass
+    d = DataFile(DATA_FILES[0])
+    d.head(1)
+class DataFile:
+    def __init__(self, filename):
+        self.filename = filename
 
+    def head(self,n_lines=10):
+        with open(self.filename) as f:
+            counter = 0
+            while counter < n_lines:
+                print f.next()
+                counter +=1
 
 if __name__ == "__main__":
     sys.exit(__main())
